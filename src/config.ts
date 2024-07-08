638a4da9429b0b56d52a3f457f800a0464615ef0
[244852437] <[244852437@qq.com]>
2024-07-08 14:51:28 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'ykLannn',
-  subtitle: 'Enjoy LAZYiNG',
+  subtitle: 'ENJOY LAZYiNG',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 100,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -44,7 +44,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Lanyk',
-  bio: 'Enjoy LAZYiNG.',
+  bio: 'ENJOY LAZYiNG.',
   links: [
     {
       name: 'GitHub',
