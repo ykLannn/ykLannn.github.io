c51eea37142d99ed7f7e81fc3b935b31942beb12
[244852437] <[244852437@qq.com]>
2024-07-09 16:44:25 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -20,6 +20,9 @@ export const siteConfig: SiteConfig = {
     position: 'center', // Equivalent to object-position, defaults center
   },
   favicon: [    // Leave this array empty to use the default favicon
+    {
+      src:'../public/favicon/favicon.jpg'
+    }
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
