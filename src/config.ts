cb98b50d55ad6f6dcb0399f7ec908e4626a9c01c
[244852437] <[244852437@qq.com]>
2024-07-09 21:58:19 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -26,9 +26,6 @@ export const siteConfig: SiteConfig = {
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
-    {
-      src: '/src/assets/images/logo.png'
-    }
   ],
 }
 
