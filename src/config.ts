cb79fedd34abc095faac1ada14634386ed0cc40e
[244852437] <[244852437@qq.com]>
2024-07-09 21:49:57 +0800

    theme

--- src/config.ts
+++ src/config.ts
@@ -9,31 +9,32 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'ykLannn',
   subtitle: 'ENJOY LAZYiNG',
-  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  lang: 'zh_CN', // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 120,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: true,     // Hide the theme color picker for visitors
+    hue: 120, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true, // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
-    src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
     {
-      src:'/public/favicon/light.png',
-      theme:'light'
+      src: '../public/favicon/light.png',
+      theme: 'light',
     },
     {
-      src:'/public/favicon/dark.png',
-      theme:'dark'
-    }
-  ]
+      src: '../public/favicon/dark.png',
+      theme: 'dark',
+    },
+  ],
 }
 
 export const navBarConfig: NavBarConfig = {
@@ -43,14 +44,14 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/ykLannn',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      url: 'https://github.com/ykLannn', // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/avatar.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Lanyk',
   bio: 'ENJOY LAZYiNG.',
   links: [
@@ -61,9 +62,9 @@ export const profileConfig: ProfileConfig = {
     },
     {
       name: 'QQ',
-      icon: 'fa6-brands:qq',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
+      icon: 'fa6-brands:qq', // Visit https://icones.js.org/ for icon codes
+      // You will need to install the corresponding icon set if it's not already included
+      // `pnpm add @iconify-json/<icon-set-name>`
       url: 'https://qm.qq.com/q/cnoKJmbbKE',
     },
     {
@@ -71,7 +72,6 @@ export const profileConfig: ProfileConfig = {
       icon: 'fa6-brands:steam',
       url: 'https://steamcommunity.com/profiles/76561198929108039/',
     },
-    
   ],
 }
 
