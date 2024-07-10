bf1267e475cc36304658df703b29861855b920a3
[244852437] <[244852437@qq.com]>
2024-07-10 11:58:27 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -22,12 +22,12 @@ export const siteConfig: SiteConfig = {
   favicon: [
     // Leave this array empty to use the default favicon
     {
-      src: '/favicon/icon.png', // Path of the favicon, relative to the /public directory
+      src: '/favicon/icon-light.png', // Path of the favicon, relative to the /public directory
       theme: 'light', // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
       //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     },
     {
-      src: '/favicon/icon.png',
+      src: '/favicon/icon-dark.png',
       theme: 'dark',
     },
   ],
